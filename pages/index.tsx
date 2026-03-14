c78b48a3888c67f849bfa958d514f5f5ddf388d5
vaishalichandane <vaishalichandane543@gmail.com>
2026-03-14 15:04:17 +0530

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Task 2 - Homepage</h1>
+      <p>This is your Task 2 page</p>
+    </div>
+  )
+}
